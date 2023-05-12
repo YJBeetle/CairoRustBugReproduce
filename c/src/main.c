bae216e372e4eb1752b70a8706586ea9d1777043
YJBeetle <yjbeetle@gmail.com>
2023-05-12 21:05:31 +0800

    init

---
+++ c/src/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <cairo.h>
+#include <cairo-pdf.h>
+
+cairo_status_t write_data(void *closure, const unsigned char *data, unsigned int length)
+{
+    FILE *file = (FILE *)closure;
+    fwrite(data, 1, length, file);
+    return CAIRO_STATUS_SUCCESS;
+}
+
+void paint(cairo_surface_t *surface)
+{
+    cairo_t *cr = cairo_create(surface);
+
+    cairo_surface_t *surface2 = cairo_recording_surface_create(CAIRO_CONTENT_COLOR_ALPHA, NULL);
+    cairo_t *cr2 = cairo_create(surface2);
+    cairo_set_source_rgb(cr2, 0., 1., 0.);
+    cairo_rectangle(cr2, 0., 0., 200., 200.);
+    cairo_fill(cr2);
+    cairo_set_source_surface(cr, surface2, 100., 100.);
+    cairo_paint(cr);
+
+    cairo_surface_t *surface3 = cairo_recording_surface_create(CAIRO_CONTENT_COLOR_ALPHA, NULL);
+    cairo_t *cr3 = cairo_create(surface3);
+    cairo_set_source_rgb(cr3, 0., 0., 1.);
+    cairo_rectangle(cr3, 0., 0., 200., 200.);
+    cairo_fill(cr3);
+    cairo_set_source_surface(cr, surface3, 200., 200.);
+    cairo_paint(cr);
+}
+
+int main()
+{
+    {
+        printf("create PNG\n");
+        cairo_surface_t *surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 500, 500);
+        paint(surface);
+        FILE *file = fopen("out.png", "wb");
+        cairo_surface_write_to_png_stream(surface, write_data, (void *)file);
+    }
+    {
+        printf("create PDF\n");
+        FILE *file = fopen("out.pdf", "wb");
+        cairo_surface_t *surface = cairo_pdf_surface_create_for_stream(write_data, (void *)file, 500., 500.);
+        paint(surface);
+        cairo_surface_finish(surface);
+    }
+}
---
+++ rust/src/main.rs
@@ -0,0 +1,35 @@
+use cairo::{Context, ImageSurface, PdfSurface, RecordingSurface, Surface};
+
+fn paint(surface: &Surface) {
+    let cr = Context::new(surface).unwrap();
+
+    let surface2 = RecordingSurface::create(cairo::Content::Alpha, None).unwrap();
+    let cr2 = Context::new(&surface2).unwrap();
+    cr2.set_source_rgb(0., 1., 0.);
+    cr2.rectangle(0., 0., 200., 200.);
+    cr2.fill().unwrap();
+    cr.set_source_surface(&surface2, 100., 100.).unwrap();
+    cr.paint().unwrap();
+
+    let surface3 = RecordingSurface::create(cairo::Content::Alpha, None).unwrap();
+    let cr3 = Context::new(&surface3).unwrap();
+    cr3.set_source_rgb(0., 0., 1.);
+    cr3.rectangle(0., 0., 200., 200.);
+    cr3.fill().unwrap();
+    cr.set_source_surface(&surface3, 200., 200.).unwrap();
+    cr.paint().unwrap();
+}
+
+fn main() {
+    println!("create PNG");
+    let surface = ImageSurface::create(cairo::Format::ARgb32, 500, 500).unwrap();
+    paint(&surface);
+    let mut f = std::fs::File::create("out.png").unwrap();
+    surface.write_to_png(&mut f).unwrap();
+
+    println!("create PDF");
+    let f = std::fs::File::create("out.pdf").unwrap();
+    let surface = PdfSurface::for_stream(500., 500., f).unwrap();
+    paint(&surface);
+    surface.finish();
+}
